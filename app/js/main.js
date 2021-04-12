9c3a0bb417db1bcb4e5620289111f14190ace26a
terrokt <terrokt@outlook.com>
2021-04-12 22:10:05 +0300

    Начал блок тренері

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-
-})
+  $('.hero__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2500,
+  });
+});
